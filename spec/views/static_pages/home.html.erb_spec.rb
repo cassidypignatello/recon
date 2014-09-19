c2d5041f892f289f2ebec430c852230b1e6d0d7b
cassidypignatello <cassidy.pignatello@gmail.com>
2014-09-19 17:21:57 -0400

    add full title helper

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'Home Page' do
   it "displays the right title" do
     visit root_path
-    expect(page).to have_title("Home | Recon")
+    expect(page).to have_title("Recon")
   end
 end
